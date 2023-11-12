adf8b68f6021928077731a0c805e31ee5d1c42e5
Your Name <you@example.com>
2023-11-12 12:02:47 +0300

    edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'src'),
+        },
+        compress: true,
+        port: 9000,
+      },
     module: {
         rules: [
             {
